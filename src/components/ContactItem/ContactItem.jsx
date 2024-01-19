33afb2f151596f413c011403fcca3c84d9b67dcc
TokarevArtemV <tokarev.artem.v@gmail.com>
2024-01-19 13:21:52 +0200

    fix item

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,17 +1,10 @@
 import css from 'components/ContactItem/ContactItem.module.css';
 
-export const ContactItem = ({
-  contact: { id, name, number },
-  onClickDelBtn,
-}) => (
+export const ContactItem = ({ contact: { name, number }, onClickDelBtn }) => (
   <li className={css.item}>
     <span className={css.text}> {`${name}: `}</span>
     <span className={css.text}> {`${number}`}</span>
-    <button
-      className={css.button}
-      type="button"
-      onClick={() => onClickDelBtn(id)}
-    >
+    <button className={css.button} type="button" onClick={onClickDelBtn}>
       Delete contact
     </button>
   </li>
